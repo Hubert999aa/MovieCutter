5d9722cfb37e1885cc8c411f8b2b954b28074d1b
Hubert Golebiowski <golab.mlody@gmail.com>
2026-01-22 16:31:55 +0100

    Add instalation files

---
+++ Persistance/PersistanceInstallation.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Persistance
+{
+    public static class PersistanceInstallation
+    {
+        public static IServiceCollection AddApplicationLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
