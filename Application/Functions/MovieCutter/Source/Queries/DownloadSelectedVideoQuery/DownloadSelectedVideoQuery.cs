3188fe5418f0a34bc65c3ad181da46e84ff26821
Hubert Golebiowski <golab.mlody@gmail.com>
2026-01-23 23:57:23 +0100

    Add download video query

---
+++ Application/Functions/MovieCutter/Source/Queries/DownloadSelectedVideoQuery/DownloadSelectedVideoQuery.cs
@@ -0,0 +1,10 @@
+﻿using Application.Mediator;
+using Domain.TechnicalModels;
+
+namespace Application.Functions.MovieCutter.Source.Queries.DownloadSelectedVideoQuery
+{
+    public class DownloadSelectedVideoQuery : IRequest<BaseResponse>
+    {
+        public required string Url { get; set; }
+    }
+}
---
+++ Application/Functions/MovieCutter/Source/Queries/DownloadSelectedVideoQuery/DownloadSelectedVideoQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using Application.Mediator;
+using Domain.ConsumersContracts;
+using Domain.TechnicalModels;
+using MassTransit;
+using Microsoft.Extensions.Options;
+
+namespace Application.Functions.MovieCutter.Source.Queries.DownloadSelectedVideoQuery
+{
+    public class DownloadSelectedVideoQueryHandler(IPublishEndpoint _publishEndpoint, IOptions<FolderPathsOptions> _folderPathsOptions) : IRequestHandler<DownloadSelectedVideoQuery, BaseResponse>
+    {
+        public async Task<BaseResponse> Handle(DownloadSelectedVideoQuery request, CancellationToken cancellationToken)
+        {
+            var message = new DownloadVideoMessage
+            {
+                Url = request.Url,
+                OutputFolder = _folderPathsOptions.Value.DownloadFolderPath,
+            };
+
+            await _publishEndpoint.Publish(message, cancellationToken);
+
+            return new BaseResponse();
+        }
+    }
+}
